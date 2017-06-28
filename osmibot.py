ffe44a8c510865a3ba50f702d018400582b06cb6
billyg1125@gmail.com <billyg1125@gmail.com>
2017-06-29 00:07:18 +0900

    Added preliminary shutdown (.die) and ping commands

--- osmibot.py
+++ osmibot.py
@@ -1,3 +1,4 @@
+import sys
 import discord
 from discord.ext import commands
 import asyncio
@@ -10,12 +11,19 @@ client = discord.Client()
 
 token = 'MjQ4NTU2OTU5NTIxODMyOTYy.DDVLlQ.z0CLKs6JT2Nc7wZOfkyntYcOli4'
 
-print('Logging in...')
-
 @bot.event
 async def on_ready():
-    print('Logged in as ' + bot.user.name + ' (ID: ' + bot.user.id + ',) on ' + str(len(client.servers)) + ' servers.')
+    print('Logged in as ' + bot.user.name + ' (ID: ' + bot.user.id + ') on ' + str(len(client.servers)) + ' servers.')
     await bot.change_presence(game=discord.Game(name="v2.0"))
 
-#Token goes below.
+@bot.command()
+async def die():
+    await bot.say(':wave:')
+    await client.logout()
+    sys.exit()
+
+@bot.command()
+async def ping():
+    await bot.say('Pong!')
+
 bot.run(token)
