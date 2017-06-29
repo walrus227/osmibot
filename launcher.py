33a96c9b6ece15ac7e68e99ce745bd0b74bb4d04
billyg1125@gmail.com <billyg1125@gmail.com>
2017-06-29 13:11:25 +0900

    Added Launcher Option for automatically configuring config.json, will complete in next commit

--- launcher.py
+++ launcher.py
@@ -4,6 +4,7 @@ import os
 import platform
 import pip
 import time
+import json
 
 useros = platform.system()
 var = 1
@@ -18,7 +19,8 @@ while var == 1:
     print('1. Run Osmibot')
     print('2. Run Osmibot with autorestart in case of errors.')
     print('3. Install dependencies')
-    print('4. Exit')
+    print('4. Configure Bot')
+    print('5. Exit')
     choice = input('Enter option number and press enter:')
     if choice == '1':
         import osmibot
@@ -33,8 +35,13 @@ while var == 1:
         install('discord.py')
 
     if choice == '4':
+        print('currently unimplemented')
+        time.sleep(3)
+
+    if choice == '5':
         sys.exit()
 
     if choice == '69':
+        os.system('cls')
         print('Commencing Countdown, engines on...')
         time.sleep(2)
